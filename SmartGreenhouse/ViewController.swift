0a808f6d58d095837773c646cb22d6078032b435
William Henlin <whenlin45@gmail.com>
2019-02-12 13:24:58 -0500

    Initial Commit

---
+++ SmartGreenhouse/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmartGreenhouse
+//
+//  Created by WHenlin on 2019-02-12.
+//  Copyright © 2019 SmartGreenhouse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
